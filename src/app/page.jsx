31ceb3dddb76f543c91169456b8fd659cf4326e1
Jason Esguerra <92497033+jasonesg@users.noreply.github.com>
2026-06-19 22:34:06 -0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="w-full max-w-full px-[20px] mx-auto transition-colors duration-300 lg:max-w-[690px]">
         <div className="pt-[80px] pb-[50px] lg:pt-[40px]">
           <header>
-            <h1 className="text-[2em] font-bold tracking-tight mb-[30px] leading-[1.2]">Hello, i'm @jasonesg</h1>
+            <h1 className="text-[2em] font-bold tracking-tight mb-[30px] leading-[1.2]">Jason Esguerra</h1>
           </header>
 
           <main role="main">
