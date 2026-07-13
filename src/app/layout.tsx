a3ad6f70fae6690ad70a9a05a0b996594306c7d3
Jason Esguerra <jasoncarloesg@gmail.com>
2026-07-13 13:33:50 -0700

    set metadataBase to jasonesg.la

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://jasonesg.la"),
   title: "Jason Esguerra",
   description: "New day, Houdys?",
   icons: {
